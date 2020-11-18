d97a778b192ccaebb81066f0fa2bc2025238775d
Ahmad Cheers <acheers1@gmail.com>
2020-11-18 21:27:46 +0100

    Built models, routes, accurate database, and controllers for restaurant review app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+piggly = Restaurant.create(name: 'piggly wiggly', address: '123 main lane', category: 'french')
+yard_house = Restaurant.create(name: 'yard house', address: '456 brick street', category: 'italian')
+vapis = Restaurant.create(name: 'vapianos', address: '23 jump street', category: 'italian')
+benihana = Restaurant.create(name: 'benihana', address: '123 hibachi lane', category: 'japanese')
+agave = Restaurant.create(name: 'agave', address: '666 memorial lane', category: 'chinese')
